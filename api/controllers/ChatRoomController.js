69d4933b06ef6e69912075ad88b7d230fac7e7e2
Peranikov <ym.nekoze@gmail.com>
2014-04-01 23:36:40 +0900

    modified chatroom

--- api/controllers/ChatRoomController.js
+++ api/controllers/ChatRoomController.js
@@ -23,11 +23,7 @@ module.exports = {
    *    `/chatroom/room`
    */
    room: function (req, res) {
-    
-    // Send a JSON response
-    return res.json({
-      hello: 'world'
-    });
+    res.view();
   },
 
 
